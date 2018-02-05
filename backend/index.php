fa3f9e8b72eee998b8db3423ae449a43f25b9097
関口 司 <t14159265358979323846@gmail.com>
2018-02-05 10:03:50 +0900

    dockerのlinkオプションが非推奨だったのでnetworksに変更した

--- backend/index.php
+++ backend/index.php
@@ -1,3 +1,19 @@
 <?php
 
-echo 'Hello backend!';
+echo 'Hello Backend!!!<br>';
+
+try {
+    $dbName = 'test_db';
+    $dbHost = 'app-db';
+    $dbPort = '3306';
+    $dbUser = 'root';
+    $dbPass = 'root';
+    $sql = new PDO (
+        "mysql:dbname={$dbName}; host={$dbHost};port={$dbPort}",
+        'root',
+        'root' );
+    echo 'DB接続に成功しました。';
+} catch ( PDOException $e ) {
+    echo "接続エラー:{$e->getMessage()}. <br>test_dbを作成していない場合は作成して下さい";
+}
+$sql = null;
